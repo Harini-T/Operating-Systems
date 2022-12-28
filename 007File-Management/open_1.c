10a9483080ff55573d3758ce690fb67b19791cff
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2022-12-28 12:17:45 +0530

    open()

--- 007File-Management/open_1.c
+++ 007File-Management/open_1.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<errno.h>
 
 void main(){
-    int fd;
-    fd = open("startup", O_RDONLY);
-    if (fd == -1){
-        printf("\n open() was failed - errno = (%d)\n",errno);
-        perror("ERROR:");
-    }
-    else
-    {
-        printf("\n open() system call executed successfully\n");
-    }
-
+	int fd;
+	fd = open("startup",O_RDONLY);
+	if(fd==-1){
+		printf("\n open() was failed - errno = (%d)\n",errno);
+		perror("ERROR:");
+	}
+	else{
+		printf("\n open() system call executed successfully\n");
+	}
 }
--- 007File-Management/open_3.c
+++ 007File-Management/open_3.c
@@ -5,15 +5,13 @@
 #include <errno.h>
 
 void main(){
-    int fd;
-    fd = open("newFile2.log", O_RDWR | O_CREAT | O_EXCL, 0774);
-    if (fd == -1){
-        printf("\n open() was failed - errno = (%d)\n",errno);
-        perror("ERROR:");
-    }
-    else
-    {
-        printf("\n open() system call executed successfully\n");
-    }
-
+	int fd;
+	fd=open("newFile2.log",O_RDWR|O_CREAT|O_EXCL,0774);
+	if(fd==-1){
+		printf("\n open() was failed - errno = (%d)\n",errno);
+		perror("Error:");
+	}
+	else{
+		printf("\n open() system call executed successfully\n");
+	}
 }
