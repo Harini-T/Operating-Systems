b8c521518ea703387ae8fcb850a1190d0e714ce4
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-03 12:35:23 +0530

    RecordLock

---
+++ 008_IPC/RecordLock/lowrcsltr.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+int main(){
+	struct flock v;
+	char a[] = "abcdefghijklmnopqr";
+	int i,fd;
+	
+	fd=open("data",O_RDWR | O_CREAT |O_APPEND,0666);
+	
+	v.l_type=F_WRLCK;
+	v.l_whence=SEEK_SET;
+	v.l_start=0;
+	v.l_len=0;
+	
+	printf("Before\n");
+	fcntl(fd,F_SETLKW,&v);
+	printf("After\n");
+	
+	for(i=0;a[i];i++){
+		write(fd,&a[i],1);
+		sleep(1);
+	}
+	v.l_type=F_UNLCK;
+	fcntl(fd,F_SETLKW,&v);
+	printf("Done\n");
+
+}
